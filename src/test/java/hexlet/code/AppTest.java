0b09f4df81602e267b9badc8d84e0b225800856c
AlexeiAK <aak3xx@gmail.com>
2022-03-14 15:05:58 +0300

    tests: Differ#generate; refactor paths
    add test for Differ#generate without specified format parameter

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -7,22 +7,43 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 class AppTest {
-    private static final String JSON_FILE_1 = "src/test/resources/file1.json";
-    private static final String JSON_FILE_2 = "src/test/resources/file2.json";
+    private static final String JSON_FILE_1 = makeFullPathForTestFile("file1.json");
+    private static final String JSON_FILE_2 = makeFullPathForTestFile("file2.json");
 
-    private static final String YML_FILE_1 = "src/test/resources/file1.yml";
-    private static final String YML_FILE_2 = "src/test/resources/file2.yml";
+    private static final String YML_FILE_1 = makeFullPathForTestFile("file1.yml");
+    private static final String YML_FILE_2 = makeFullPathForTestFile("file2.yml");
 
     private final String plainFormat = "plain";
     private final String stylishFormat = "stylish";
     private final String jsonFormat = "json";
+
+    private static String makeFullPathForTestFile(String fileName) {
+        return "src/test/resources/" + fileName;
+    }
+
+    private static String makeFullPathForFixtureFile(String fileName) {
+        return "src/test/resources/fixtures/" + fileName;
+    }
+
+    @Test
+    void whenFilesWithoutSpecifiedFormatThenDefaultFormat() throws Exception {
+        String actualJsons = Differ.generate(JSON_FILE_1, JSON_FILE_2);
+        String actualYmls = Differ.generate(YML_FILE_1, YML_FILE_2);
+
+        String stylishPath = makeFullPathForFixtureFile(stylishFormat);
+        String expected = Files.readString(Paths.get(stylishPath));
+
+        Assertions.assertEquals(expected, actualJsons);
+        Assertions.assertEquals(expected, actualYmls);
+    }
 
     @Test
     void whenFilesThenStylish() throws Exception {
         String actualJsons = Differ.generate(JSON_FILE_1, JSON_FILE_2, stylishFormat);
         String actualYmls = Differ.generate(YML_FILE_1, YML_FILE_2, stylishFormat);
 
-        String expected = Files.readString(Paths.get("src/test/resources/fixtures/stylish"));
+        String stylishPath = makeFullPathForFixtureFile(stylishFormat);
+        String expected = Files.readString(Paths.get(stylishPath));
 
         Assertions.assertEquals(expected, actualJsons);
         Assertions.assertEquals(expected, actualYmls);
@@ -33,7 +54,8 @@ class AppTest {
         String actualJsons = Differ.generate(JSON_FILE_1, JSON_FILE_2, plainFormat);
         String actualYmls = Differ.generate(YML_FILE_1, YML_FILE_2, plainFormat);
 
-        String expected = Files.readString(Paths.get("src/test/resources/fixtures/plain"));
+        String plainPath = makeFullPathForFixtureFile(plainFormat);
+        String expected = Files.readString(Paths.get(plainPath));
 
         Assertions.assertEquals(expected, actualJsons);
         Assertions.assertEquals(expected, actualYmls);
@@ -44,7 +66,8 @@ class AppTest {
         String actualJsons = Differ.generate(JSON_FILE_1, JSON_FILE_2, jsonFormat);
         String actualYmls = Differ.generate(YML_FILE_1, YML_FILE_2, jsonFormat);
 
-        String expected = Files.readString(Paths.get("src/test/resources/fixtures/json"));
+        String jsonPath = makeFullPathForFixtureFile(jsonFormat);
+        String expected = Files.readString(Paths.get(jsonPath));
 
         Assertions.assertEquals(expected, actualJsons);
         Assertions.assertEquals(expected, actualYmls);
